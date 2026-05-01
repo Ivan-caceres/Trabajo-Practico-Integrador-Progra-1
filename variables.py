39e9c59421da329053c20031b43f78c93e3d4653
Ivan Caceres <ezeivan98@gmail.com>
2026-05-01 16:26:09 -0300

    Editar variables.py

--- variables.py
+++ variables.py
@@ -1,9 +1,5 @@
 from funciones import (calcular_riesgo, calcular_estado_sv, calcular_carga)
-
-uso_cpu = 50 #esto conecta con los input
-uso_ram = 60 #esto conecta con los input
-espacio_libre_gb = 200 #esto conecta con los input
-estado_firewall = "Activo" #esto conecta con los input
+from inputs import(uso_cpu, uso_ram, espacio_libre_disco, cantidad_procesos_activos, estado_firewall)
 
 
 porcentaje_total_carga =  (uso_cpu + uso_ram) / 2
@@ -11,7 +7,7 @@ porcentaje_total_carga =  (uso_cpu + uso_ram) / 2
 nivel_estimado_riesgo = calcular_riesgo(estado_firewall, porcentaje_total_carga)
 
 ram_disponible_porcentaje = 100 - uso_ram
-recursos_disponibles = f"Disco: {espacio_libre_gb}GB \nRam libre: {ram_disponible_porcentaje}%"
+recursos_disponibles = f"Disco: {espacio_libre_disco}GB \nRam libre: {ram_disponible_porcentaje}%"
 
 estado_general_servidor = calcular_estado_sv(nivel_estimado_riesgo)
 
