b9852eae3532209edbf94dab4d6ced763629c21b
Fabricio <fabriciomaidanaa14@gmail.com>
2026-05-02 19:02:20 -0300

    Correccion

--- Clase 1/Ejercicio_2.py
+++
@@ -1,5 +0,0 @@
-numero_decimal = float(input("Ingrese un número decimal: "))
-
-print("El número ingresado es: ", numero_decimal)
-print("El número ingresado es: ", int(numero_decimal))
-print("El número ingresado es: ", str(numero_decimal))
